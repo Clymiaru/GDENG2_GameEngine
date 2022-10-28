9f58cd7a9a65b1c17eada24ba3c286d6ac85dc62
Clymiaru <sekkadica@gmail.com>
2022-10-28 11:15:00 +0800

    Refactored Math: Color and Mat4

--- Engine/Source/Engine/Math/Color.h
+++ Engine/Source/Engine/Math/Color.h
@@ -1,9 +1,97 @@
 ﻿#pragma once
+#include <DirectXMath.h>
 
 namespace Engine
 {
-	struct Color
+	struct Vector3Float;
+
+	struct Color : public DirectX::XMFLOAT4
 	{
-	
+		explicit Color();
+
+		explicit Color(float red,
+		               float green,
+		               float blue);
+
+		explicit Color(float red,
+		               float green,
+		               float blue,
+		               float alpha);
+
+		explicit explicit Color(const Vector3Float& rgbColor);
+
+		explicit Color(const XMFLOAT4& rgbaColor);
+
+		Color(const Color&) = default;
+
+		Color& operator=(const Color&) = default;
+
+		Color(Color&&) = default;
+
+		Color& operator=(Color&&) = default;
+
+		explicit operator DirectX::XMVECTOR() const;
+
+		bool operator ==(const Color& c) const;
+
+		bool operator !=(const Color& c) const;
+
+		Color& operator+=(const Color& c);
+
+		Color& operator-=(const Color& c);
+
+		Color& operator*=(const Color& c);
+
+		Color& operator*=(float s);
+
+		Color& operator/=(const Color& c);
+
+		Color operator+() const
+		{
+			return *this;
+		}
+
+		Color operator-() const;
+
+		[[nodiscard]]
+		float Red() const;
+
+		void Red(float red);
+
+		[[nodiscard]]
+		float Green() const;
+
+		void Green(float green);
+
+		[[nodiscard]]
+		float Blue() const;
+
+		void Blue(float blue);
+
+		[[nodiscard]]
+		float Alpha() const;
+
+		void Alpha(float alpha);
+
+		static Color Lerp(const Color& c1,
+		                  const Color& c2,
+		                  float t) noexcept;
 	};
+
+	Color operator+(const Color& c1,
+	                const Color& c2) noexcept;
+
+	Color operator-(const Color& c1,
+	                const Color& c2) noexcept;
+
+	Color operator*(const Color& c1,
+	                const Color& c2) noexcept;
+
+	Color operator*(const Color& c,
+	                float s) noexcept;
+
+	Color operator/(const Color& c1,
+	                const Color& c2) noexcept;
+
+	// struct Color32 for char based color (255, 255, 255, 255)
 }
--- Engine/Source/Engine/Math/Matrix4.h
+++ Engine/Source/Engine/Math/Matrix4.h
@@ -1,11 +1,72 @@
 ﻿#pragma once
+#include <DirectXMath.h>
 
 namespace Engine
 {
-	// DirectX::XMMATRIX
-	class Matrix4
-	{
-	public:
+	struct Vector3Float;
 	
+	// DirectX::XMMATRIX
+	// Assume for float values for now
+	// As the typical use case of Matrix4 is for transformations
+	struct Matrix4 : public DirectX::XMFLOAT4X4
+	{
+		explicit Matrix4();
+
+		explicit Matrix4(const Vector3Float& r0,
+						const Vector3Float& r1,
+		                const Vector3Float& r2);
+
+		Matrix4(const Matrix4&) = default;
+		Matrix4& operator=(const Matrix4&) = default;
+
+		Matrix4(Matrix4&&) = default;
+		Matrix4& operator=(Matrix4&&) = default;
+
+		operator DirectX::XMMATRIX() const;
+
+		bool operator == (const Matrix4& m) const noexcept;
+		bool operator != (const Matrix4& m) const noexcept;
+
+		Matrix4& operator+= (const Matrix4& m) noexcept;
+		Matrix4& operator-= (const Matrix4& m) noexcept;
+		Matrix4& operator*= (const Matrix4& m) noexcept;
+		Matrix4& operator*= (float s) noexcept;
+		Matrix4& operator/= (float s) noexcept;
+
+		// Element-wise divide
+		Matrix4& operator/= (const Matrix4& m) noexcept;
+
+		Matrix4 operator+() const noexcept { return *this; }
+		Matrix4 operator-() const noexcept;
+
+		Matrix4 Transpose() const noexcept;
+
+		float Determinant() const noexcept;
+
+		static Matrix4 CreateTranslation(const Vector3Float& position) noexcept;
+		static Matrix4 CreateTranslation(float x, float y, float z) noexcept;
+
+		static Matrix4 CreateScale(const Vector3Float& scales) noexcept;
+		static Matrix4 CreateScale(float x, float y, float z) noexcept;
+		static Matrix4 CreateScale(float scale) noexcept;
+
+		static Matrix4 CreateRotationX(float radians) noexcept;
+		static Matrix4 CreateRotationY(float radians) noexcept;
+		static Matrix4 CreateRotationZ(float radians) noexcept;
+
+		// static Matrix CreatePerspectiveFieldOfView(float fov, float aspectRatio, float nearPlane, float farPlane) noexcept;
+		// static Matrix CreatePerspective(float width, float height, float nearPlane, float farPlane) noexcept;
+		// static Matrix CreatePerspectiveOffCenter(float left, float right, float bottom, float top, float nearPlane, float farPlane) noexcept;
+		// static Matrix CreateOrthographic(float width, float height, float zNearPlane, float zFarPlane) noexcept;
+		// static Matrix CreateOrthographicOffCenter(float left, float right, float bottom, float top, float zNearPlane, float zFarPlane) noexcept;
 	};
+
+	Matrix4 operator+ (const Matrix4& m1, const Matrix4& m2) noexcept;
+	Matrix4 operator- (const Matrix4& m1, const Matrix4& m2) noexcept;
+	Matrix4 operator* (const Matrix4& m1, const Matrix4& m2) noexcept;
+	Matrix4 operator* (const Matrix4& m, float s) noexcept;
+	Matrix4 operator/ (const Matrix4& m, float s) noexcept;
+	
+	// Element-wise divide
+	Matrix4 operator/ (const Matrix4& m1, const Matrix4& m2) noexcept;
 }
